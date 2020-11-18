246aa2e3f4d170302853831dcf825b9ff0379c32
Marc Toussaint <toussaint@tu-berlin.de>
2020-11-18 19:41:11 +0100

    benchmark via py

---
+++ Makefile
@@ -0,0 +1,22 @@
+BASE = rai
+
+target: build bin
+
+DEPEND = Core Optim ry
+
+build: $(DEPEND:%=inPath_makeLib/%)
+
+bin:
+	+make -C rai bin
+
+installUbuntuAll: $(DEPEND:%=inPath_installUbuntu/%)
+
+printUbuntuAll: $(DEPEND:%=inPath_printUbuntu/%) printUbuntu
+
+clean: $(DEPEND:%=inPath_clean/%)
+
+dependAll: cleanLocks cleanDepends $(DEPEND:%=inPath_depend/%)
+
+include $(BASE)/build/generic.mk
+
+.NOTPARALLEL:
---
+++ config.mk
@@ -0,0 +1,35 @@
+# In this file you can unset dependencies, e.g.
+# 
+# LAPACK=0 #to avoid need to include/link to LAPACK
+# PHYSX=0 #to avoid linking to Physx (Nvidia simulator)
+# GTK = 0 #to avoid linking to GTK
+# etc
+#
+
+# only UNCOMMENT some of the following lines
+# (they are already set =1 in the components that depend on them)
+
+#OPTIM = debug
+#OPTIM = fast_debug
+
+#GL = 0
+OPENCV = 0
+GRAPHVIZ = 0
+GTK = 0
+G4 = 0
+#PYBIND = 0
+PNG = 0
+
+#EIGEN = 0
+#CERES = 0
+#NLOPT = 0
+#IPOPT = 0
+
+#FCL = 0
+PCL = 0
+ODE = 0
+PHYSX = 0
+BULLET = 0
+
+ROS = 0
+
